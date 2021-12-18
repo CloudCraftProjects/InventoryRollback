866f6da0f49b193faedad18c6f1eb770106caf99
booky10 <boooky10@gmail.com>
2021-12-18 21:36:51 +0100

    Updated to 1.18.1 and moved to gradle kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "me.danjono"
+version = "1.5.1"
+
+repositories {
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    api("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifactId = project.name.toLowerCase()
+        from(components["java"])
+    }
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
+}
